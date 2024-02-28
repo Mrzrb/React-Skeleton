46df9d16882dd5bd6f29c08acf8f6b0853d00254
zhangruobin <zhangruobin@zuoyebang.com>
2024-02-28 19:00:04 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 import { useQuery } from "react-query";
 
 function App() {
-    const ff = async () => {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(555)
-            }, 1000);
-        });
-    }
-    const { data, isLoading, isError } = useQuery('posts', () => ff())
-
-    if (isLoading) {
-        return <div>Loading...</div>
-    }
-    if (isError) {
-        return <div>Error</div>
-    }
-
     return (
         <div className="App rounded-md">
-            {isLoading ? "true" : "false"}--{JSON.stringify(data)}
+
         </div>
     );
 }
